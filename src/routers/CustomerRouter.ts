ee2260b2746c80aa56cc7f2b61bba07f1f3046f0
Lorenzo Roman <alu0101314990@ull.edu.es>
2024-05-09 22:01:12 +0000

    .

--- src/routers/CustomerRouter.ts
+++ src/routers/CustomerRouter.ts
@@ -16,17 +16,33 @@ CustomerRouter.post('/customers', async (req: Request, res: Response) => {
 
 // Leer todos los clientes
 CustomerRouter.get('/customers', async (req: Request, res: Response) => {
-  try {
-    const customers = await Customer.find();
-    res.send(customers);
-  } catch (error) {
-    res.status(500).send(error);
+  // si en el query string se encuentra el parámetro nif se busca por nif
+  if (req.query.nif) {
+    const nif = req.query.nif;
+    try {
+      const customer = await Customer.findOne({ nif });
+      if (!customer) {
+        return res.status(404).send({ message: 'Cliente no encontrado' });
+      }
+      return res.send(customer);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  } else {
+    //si el parámetro nif no se encuentra se listan todos los clientes
+    try {
+      const customers = await Customer.find();
+      return res.send(customers);
+    } catch (error) {
+      return  res.status(500).send(error);
   }
+}
+
 });
 
 // Leer un cliente por NIF
-CustomerRouter.get('/customers', async (req: Request, res: Response) => {
-  const nif = req.query.nif;
+CustomerRouter.get('/customers/nif/:nif', async (req: Request, res: Response) => {
+  const nif = req.params.nif;
   try {
     const customer = await Customer.findOne({ nif });
     if (!customer) {
@@ -109,3 +125,4 @@ CustomerRouter.delete('/customers/id/:id', async (req: Request, res: Response) =
     return res.status(500).send(error);
   }
 });
+
