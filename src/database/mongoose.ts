cf3db1a172c9c55e5f9da3c285ce98fb2bb3f4c8
Lorenzo Roman <alu0101314990@ull.edu.es>
2024-05-09 20:49:47 +0000

    url

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -1,6 +1,6 @@
 import { connect } from 'mongoose';
 
-connect('mongodb+srv://DSIkea-rest-api:DSIkea@clusterdsikea.3wvgsf9.mongodb.net/')
+connect("mongodb+srv://DSIkea-rest-api:DSIkea@clusterdsikea.3wvgsf9.mongodb.net/")
 .then(() => {
   console.log('Connection to MongoDB server established');
 }).catch(() => {
