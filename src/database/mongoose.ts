6c7a40b98594f27ed7aec8e86875a018e20e46ea
Lorenzo Roman <alu0101314990@ull.edu.es>
2024-05-11 16:31:18 +0000

    .

--- src/database/mongoose.ts
+++ src/database/mongoose.ts
@@ -3,7 +3,7 @@ import { connect } from 'mongoose';
 
 // Connect to Database
 export let Launch = async () => {
-   await connect(process.env.MONGODB_URL!).then(() => {
+   await connect("mongodb+srv://DSIkea-rest-api:DSIkea@clusterdsikea.3wvgsf9.mongodb.net/DSIkea-api").then(() => {
     console.log('Connected to the database');
   }).catch(() => {
     console.log('Something went wrong when conecting to the database');
